940b1cd0bb0e5c341489997e63a3e649e811e620
CoderYihaoWang <yesiamyihao@gmail.com>
2020-07-16 21:37:59 +1200

    Add search button on SearchPanel

--- src/Components/SearchPanel.tsx
+++ src/Components/SearchPanel.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Switch, FormControlLabel } from '@material-ui/core';
+import { TextField, Switch, FormControlLabel, Button } from '@material-ui/core';
 import { IUserInput } from '../Common/Interfaces';
 
 interface IProps {
@@ -14,27 +14,50 @@ function SearchPanel(props: IProps) {
 
     const handleFulltime = function() {
         setFullTime(!fullTime);
+        handleSubmit();
+    }
+
+    const handleSubmit = function() {
+        let userInput : IUserInput = {
+            search: search,
+            location: location,
+            fullTime: fullTime
+        };
+        props.setUserInput(userInput);
     }
 
     return (
         <div className='SearchPanel'>
             <FormControlLabel 
-                control={<Switch checked={fullTime} onChange={handleFulltime} name='fullTime'/>}
+                control={
+                    <Switch 
+                        checked={fullTime} 
+                        onChange={handleFulltime} 
+                        name='fullTime'
+                    />
+                }
                 label='Only show full-time jobs'
             />
             <TextField
                 id='search'
                 label='Description'
                 helperText='The job description to search for (eg. Python)'
+                onChange={e => setSearch(e.target.value)}
             />
             <TextField
                 id='location'
                 label='Location'
                 helperText='The location of the location (eg. Auckland)'
+                onChange={e => setLocation(e.target.value)}
             />
-
+            <Button 
+                variant='outlined'
+                onClick={handleSubmit}
+            >
+                Search
+            </Button>
         </div>
     )
 }
 
-export default SearchPanel
+export default SearchPanel;
