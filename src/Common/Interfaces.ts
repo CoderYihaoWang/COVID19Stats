2a1c1637ad293e8e6352d72613c98ae186176d08
CoderYihaoWang <yesiamyihao@gmail.com>
2020-07-16 20:57:03 +1200

    Add types for properties

--- src/Common/Interfaces.ts
+++ src/Common/Interfaces.ts
@@ -1,5 +1,19 @@
 export interface IUserInput {
-    search: string,
-    location: string,
+    search: string | null,
+    location: string | null,
     fullTime: boolean
+}
+
+export interface IResult {
+    id: string,
+    type: 'Full Time' | 'Part Time',
+    url: string,
+    created_at: string,
+    company: string,
+    company_url: string,
+    location: string,
+    title: string,
+    description: string,
+    how_to_apply: string,
+    company_logo: string
 }
