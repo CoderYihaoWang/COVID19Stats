e114d772dded41ced2edf473518dc49aa7443ed4
CoderYihaoWang <yesiamyihao@gmail.com>
2020-07-17 21:44:58 +1200

    Add chart

--- src/Components/Chart.tsx
+++ src/Components/Chart.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Line, LineChart, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 interface IProps {
     country: string,
@@ -10,9 +11,19 @@ interface IProps {
     displayActive: boolean
 }
 
+
 export default function Chart(props: IProps) {
 
-    return (
-    <p>placeholder for chart: {props.country}</p>
+    useEffect(() => {
+        fetch('')
+    }, []);
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 400, pv: 2400, amt: 2400},{name: 'Page C', uv: 400, pv: 2400, amt: 2400}];
+    return (   
+  <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+    <XAxis dataKey="name" />
+    <YAxis />
+  </LineChart>
     )
 }
