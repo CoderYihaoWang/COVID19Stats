5b4e0080ad02517caa32ecb1f191c204fb687dd5
CoderYihaoWang <yesiamyihao@gmail.com>
2020-07-19 14:31:21 +1200

    Refine layout of country list

--- src/Components/CountryItem.tsx
+++ src/Components/CountryItem.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { ListItem, ListItemText, Tooltip, Accordion, AccordionSummary, AccordionDetails, Button, Typography, List } from '@material-ui/core';
+import { ListItem, ListItemText, Tooltip, Accordion, AccordionSummary, AccordionDetails, Button, Typography, List, makeStyles, createStyles, Theme } from '@material-ui/core';
 import { ICountryData, IColors } from '../Common/Interfaces';
 
 interface IProps {
@@ -9,7 +9,25 @@ interface IProps {
     colors: IColors
 }
 
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+        item: {
+            display: "flex",
+            justifyContent: "space-between"
+        },
+        dataHead: {
+            display: "block",
+        },
+        data: {
+            display: "block",
+            textAlign: "right"
+        }
+    })
+)
+
 export default function CountryItem(props: IProps) {
+    const classes = useStyles();
+    
     return (
         <Accordion>
             <AccordionSummary>
@@ -17,46 +35,63 @@ export default function CountryItem(props: IProps) {
             </AccordionSummary>
             <AccordionDetails>
                 <List>
-                    <ListItem>
-                        <Typography variant="body1">
-                            Totol confirmed: {props.data.TotalConfirmed}
+                    <ListItem className={classes.item}>
+                        <Typography variant="body2" className={classes.dataHead}>
+                            Totol confirmed:
+                        </Typography>
+                        <Typography variant="body2" className={classes.data}>
+                            {props.data.TotalConfirmed}
                         </Typography>
                     </ListItem>
-                    <ListItem>
-                        <Typography variant="body1">
-                            New confirmed: {props.data.NewConfirmed}
+                    <ListItem className={classes.item}>
+                        <Typography variant="body2" className={classes.dataHead}>
+                            New confirmed:
+                        </Typography>
+                        <Typography variant="body2" className={classes.data}>
+                            {props.data.NewConfirmed}
                         </Typography>
                     </ListItem>
-                    <ListItem>
-                        <Typography variant="body1">
-                            Totol recovered: {props.data.TotalRecovered}
+                    <ListItem className={classes.item}>
+                        <Typography variant="body2" className={classes.dataHead}>
+                            Totol recovered:
+                        </Typography>
+                        <Typography variant="body2" className={classes.data}>
+                            {props.data.TotalRecovered}
                         </Typography>
                     </ListItem>
-                    <ListItem>
-                        <Typography variant="body1">
-                            New recovered: {props.data.NewRecovered}
+                    <ListItem className={classes.item}>
+                        <Typography variant="body2" className={classes.dataHead}>
+                            New recovered:
+                        </Typography>
+                        <Typography variant="body2" className={classes.data}>
+                            {props.data.NewRecovered}
                         </Typography>
                     </ListItem>
-                    <ListItem>
-                        <Typography variant="body1">
-                            Totol deaths: {props.data.TotalDeaths}
+                    <ListItem className={classes.item}>
+                        <Typography variant="body2" className={classes.dataHead}>
+                            Totol deaths:
+                        </Typography>
+                        <Typography variant="body2" className={classes.data}>
+                            {props.data.TotalDeaths}
                         </Typography>
                     </ListItem>
-                    <ListItem>
-                        <Typography variant="body1">
-                            New deaths: {props.data.NewDeaths}
+                    <ListItem className={classes.item}>
+                        <Typography variant="body2" className={classes.dataHead}>
+                            New deaths:
+                        </Typography>
+                        <Typography variant="body2" className={classes.data}>
+                            {props.data.NewDeaths}
                         </Typography>
                     </ListItem>
-                    <ListItem>
+                    <ListItem className={classes.item}>
                        <Button onClick={() => {
                             props.setSlug(props.data.Slug);
                             props.setCountry(props.data.Country);
-                        }}>
-                            Show graph
+                        }} variant="outlined">
+                            show on graph
                         </Button> 
                     </ListItem>
                 </List>
-                 
             </AccordionDetails>
         </Accordion>
     )
